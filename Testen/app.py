8400ea418d51882d4405a7c89b69ca874a81cc4c
Henk van der Duim <henk.vanderduim@gmail.com>
2022-02-11 09:47:06 +0100

    Testen

--- Testen/app.py
+++ Testen/app.py
@@ -15,8 +15,8 @@ TestenStack(app, "TestenStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
+    env=cdk.Environment(account='048127819745', region='eu-central-1')
+    
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
--- Testen/testen/testen_stack.py
+++ Testen/testen/testen_stack.py
@@ -1,19 +1,36 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import CfnOutput, Stack
+import aws_cdk.aws_ec2 as ec2
 from constructs import Construct
 
 class TestenStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TestenQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.vpc = ec2.Vpc(self, "VPC1",
+                           max_azs=2,
+                           cidr="10.10.10.0/24",
+                           # configuration will create 3 groups in 2 AZs = 6 subnets.
+                           subnet_configuration=[ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PUBLIC,
+                               name="Public1",
+                               cidr_mask=25
+                           )
+                           ]
+                           )
+
+        self.vpc = ec2.Vpc(self, "VPC2",
+                           max_azs=2,
+                           cidr="10.20.20.0/24",
+                           # configuration will create 3 groups in 2 AZs = 6 subnets.
+                           subnet_configuration=[ec2.SubnetConfiguration(
+                               subnet_type=ec2.SubnetType.PUBLIC,
+                               name="Public2",
+                               cidr_mask=25
+                           )
+                           ]
+                           )
+        CfnOutput(self, "Output",
+                       value=self.vpc.vpc_id)
