485fa3fcc4d38e4dc8faa2437f89087c19ba3b14
Henk van der Duim <henk.vanderduim@gmail.com>
2022-03-18 11:35:47 +0100

    MVP v1.1 (in progress)

--- mvpscript11/app.py
+++ mvpscript11/app.py
@@ -20,7 +20,7 @@ Mvpscript11Stack(app, "Mvpscript11Stack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account="048127819745", region="eu-central-1"),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
