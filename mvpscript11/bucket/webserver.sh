564ba2d3540e58f4ea1f3e2382c9926a4565fb80
Henk van der Duim <henk.vanderduim@gmail.com>
2022-03-30 14:20:18 +0200

    MVP Final

--- mvpscript11/bucket/webserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-          yum -y install httpd
-          systemctl enable httpd
-          systemctl start httpd
-          echo '<html><h1>Joehoe dit is een WEBSERVER!</h1></html>' > /var/www/html/index.html
