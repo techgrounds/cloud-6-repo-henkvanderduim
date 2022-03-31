841e0256d8bcc66935e2b4c2ba9d19e25c96b215
Henk van der Duim <henk.vanderduim@gmail.com>
2022-03-31 09:51:21 +0200

    MVP

--- mvpfinal/tests/unit/test_mvpfinal_stack.py
+++ mvpfinal/tests/unit/test_mvpfinal_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from mvpfinal.old_code import MvpfinalStack
+from mvpfinal.mvpfinal_stack import MvpfinalStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in mvpfinal/mvpfinal_stack.py
@@ -9,7 +9,6 @@ def test_sqs_queue_created():
     app = core.App()
     stack = MvpfinalStack(app, "mvpfinal")
     template = assertions.Template.from_stack(stack)
-
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
